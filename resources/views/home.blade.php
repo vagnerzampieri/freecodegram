ea33d5d9416af859f6d688d4acb0cfcbb31fde3e
Vagner Zampieri <vgzampieri@gmail.com>
2019-09-08 16:58:09 -0300

    Create route to show profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         </div>
 
         <div class="col-9 pt-5">
-            <div><h1>freecodecamp</h1></div>
+        <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
