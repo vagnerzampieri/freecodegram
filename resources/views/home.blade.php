99c5b0b84665fc81a987a3502039c1e14e75e23d
Vagner Zampieri <vgzampieri@gmail.com>
2019-09-07 22:28:58 -0300

    Apply row layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,17 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/images/freeCodeCampLogo.jpg" alt="freeCodeCampLogo" class="rounded-circle">
+        </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
         </div>
     </div>
