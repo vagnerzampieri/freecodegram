2d25118472fc25dae3f818be3b0afc9c6b6e0d0e
Vagner Zampieri <vgzampieri@gmail.com>
2019-10-02 08:44:37 -0300

    Update image in profile

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -36,7 +36,16 @@ class ProfilesController extends Controller
             'image' => ''
         ]);
 
-        auth()->user()->profile->update($data);
+        if (request('image')) {
+            $imagePath = request('image')->store('profile', 'public');
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
+            $image->save();
+        }
+
+        auth()->user()->profile->update(array_merge(
+            $data,
+            ['image' => $imagePath]
+        ));
         return redirect("/profile/{$user->id}");
     }
 }
