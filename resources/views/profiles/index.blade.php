470822a98e6858bd70c80bf72e2552a8be7e42d0
Vagner Zampieri <vgzampieri@gmail.com>
2019-10-04 08:24:48 -0300

    Follow layout example

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,7 +9,11 @@
 
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
-                <h1>{{ $user->username }}</h1>
+                <div class="d-flex align-items-center pb-3">
+                    <div class="h4">{{ $user->username }}</div>
+
+                    <button class="btn btn-primary ml-4">Follow</button>
+                </div>
 
                 @can('update', $profile)
                     <a href="/p/create">Add New Post</a>
