4d02ab350c2dfaa37c049f0c6b39bf847f06d290
Vagner Zampieri <vgzampieri@gmail.com>
2019-10-04 08:12:19 -0300

    Fix nullable image

--- database/migrations/2019_09_08_203458_create_profiles_table.php
+++ database/migrations/2019_09_08_203458_create_profiles_table.php
@@ -19,7 +19,7 @@ class CreateProfilesTable extends Migration
             $table->string('title')->nullable();
             $table->text('description')->nullable();
             $table->string('url')->nullable();
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->timestamps();
 
             $table->index('user_id');
